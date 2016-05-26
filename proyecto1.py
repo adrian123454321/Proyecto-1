b0451a58620c937de2f6c489a2b255c5a64c8c60
adrian123454321 <adrian.lora12345@gmail.com>
2016-05-25 20:11:52 -0600

    arreglos del problema

--- proyecto1.py
+++ proyecto1.py
@@ -8,7 +8,9 @@ def prob_3 (b,p):
 	return (b**p)
 
 def prob_4 (h,l):
-	return(((l/2)*"*")+h+((l/2)*"*")
+	return(((l//2)*"*")+h+((l//2)*"*")
 
 def prob_5 (a,b),(c,d),(e,f):
-	return (a*c+a*e+c*e+b*d+b*f+d*f)
+	return (a*c+a*e+c*e+b*d+b*f+d*f)   
+
+def prob_6 
